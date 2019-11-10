e974f9604ab707a366e670ffb99153395538e7e2
deep <deep9751@gmail.com>
2019-11-10 21:53:00 +0200

    add auth to angular

--- PostAPI/ClientApp/src/app/model/user.ts
+++ PostAPI/ClientApp/src/app/model/user.ts
@@ -1,7 +1,8 @@
 export class User {
     constructor(
         public login: string,
-        public password: string
+        public password: string,
+        public token?: string
     ) { }
 
 }
